93ca0ea58659096c33b8f9aa6a2420635721d363
ahmed7761 <sharif7761@gmail.com>
2025-02-10 02:01:44 +0600

    generated services and components

---
+++ src/app/components/item-form/item-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-form',
+  imports: [],
+  templateUrl: './item-form.component.html',
+  styleUrl: './item-form.component.css'
+})
+export class ItemFormComponent {
+
+}
