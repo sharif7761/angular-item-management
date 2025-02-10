637f6bcd0233b7d1476f080e8946dee075f7fb1b
ahmed7761 <sharif7761@gmail.com>
2025-02-10 22:22:40 +0600

    added item service

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Item } from '../models/item.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ItemService {
+  private storageKey = 'items';
 
-  constructor() { }
+  getItems(): Item[] {
+    const data = localStorage.getItem(this.storageKey);
+    return data ? JSON.parse(data) : [];
+  }
+
+  saveItems(items: Item[]): void {
+    localStorage.setItem(this.storageKey, JSON.stringify(items));
+  }
+
+  addItem(item: Item): void {
+    const items = this.getItems();
+    items.push(item);
+    this.saveItems(items);
+  }
+
+  updateItem(updatedItem: Item): void {
+    const items = this.getItems().map(item =>
+      item.id === updatedItem.id ? updatedItem : item
+    );
+    this.saveItems(items);
+  }
+
+  deleteItem(id: number): void {
+    const items = this.getItems().filter(item => item.id !== id);
+    this.saveItems(items);
+  }
 }
